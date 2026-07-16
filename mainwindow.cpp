83d8260e64596e611a9f49592aa2494a3b09e532
Mahdi <mahdi41785822@gmail.com>
2026-07-16 17:39:09 +0330

    made users.json to save data inside

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,26 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QFile>
+#include <QJsonDocument>
+#include <QJsonArray>
+#include <QJsonObject>
+#include <QMessageBox>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    QFile file("users.json");
+
+    if(!file.exists()){
+        file.open(QIODevice::WriteOnly);
+        QJsonArray array;
+        file.write(QJsonDocument(array).toJson());
+        file.close();
+    }
 }
 
 MainWindow::~MainWindow()
