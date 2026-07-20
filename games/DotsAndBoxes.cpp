20595353ce79b39742734722784347df526997b7
Radman <Radman.Saghafian@gmail.com>
2026-07-21 00:00:57 +0330

    Added initial game architecture

---
+++ games/DotsAndBoxes.cpp
@@ -0,0 +1,3 @@
+#include "DotsAndBoxes.h"
+
+DotsAndBoxes::DotsAndBoxes() {}
---
+++ games/DotsAndBoxes.h
@@ -0,0 +1,10 @@
+#ifndef DOTSANDBOXES_H
+#define DOTSANDBOXES_H
+
+class DotsAndBoxes
+{
+public:
+    DotsAndBoxes();
+};
+
+#endif // DOTSANDBOXES_H
